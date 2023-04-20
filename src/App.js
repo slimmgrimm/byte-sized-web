4aeab5cf3292cdc824fc1b96b77e32e49d1a5cc9
Slimmgrimmith <dannywukasch@gmail.com>
2023-04-19 20:32:58 -0500

    Import BrowserRouter Create mockup Routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import Header from './components/Header.js';
 import Warmups from './components/Warmups.js'
@@ -7,13 +13,48 @@ import CssSection from './components/CssSection.js';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/cheatsheet">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="App">
       <Header />
       <Warmups />
       <ReactSection />
       <CssSection />
     </div>
+        <Routes>
+          <Route path="/about" element={ <About/> } />
+          <Route path="/cheatsheet" element={ <Cheatsheet /> } />
+          <Route path="/" element={ <Home/> } />
+        </Routes>
+      </div>
+    </Router>
+   
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Cheatsheet() {
+  return <h2>Cheatsheet</h2>;
 }
 
 export default App;
