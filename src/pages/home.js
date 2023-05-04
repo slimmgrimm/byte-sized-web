f61f1571e6961d2ce70b7ad5ebf68dfa1ffa7972
Slimmgrimmith <dannywukasch@gmail.com>
2023-05-03 22:43:28 -0500

    Add html submit <form>

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,19 @@ function Home() {
     return (
         <article>
         <h1>Welcome to the Home screen. Test a link for me!</h1>
+        <form action='./posts.json' method='post'>
+
+            <label for='title'>Article Title: </label>
+            <input type='text' id='title' name='title'></input>
+
+            <label for='subtitle'>Subtitle: </label>
+            <input type='text' id='subtitle' name='subtitle'></input>
+
+            <label for='text'>Body Text: </label>
+            <textarea rows='20' cols='60' id='text' name='text'></textarea>
+
+            <button type='submit'>Submit</button>
+        </form>
         </article>
     )
 }
